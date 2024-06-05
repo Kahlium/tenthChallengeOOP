a94103bed43930f4c8bec026eac3733a24d99a32
Kahlium <kahligtz@gmail.com>
2024-06-05 15:45:05 -0600

    inquirer prompts written, working on svg creation

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-class Logo {
-    constructor(text, textColor, shape, shapeColor) {
-        this.text = text;
-        this.textColor = textColor;
-        this.shape = shape;
-        this.shapeColor = shapeColor;
-    }
-}
+const inquirer = require('inquirer');
 
-class Layer {
+const questions = [
+    {
+        type: 'input',
+        message: 'Write down your logo text',
+        name: 'text'
+    },
+    {
+        type: 'input',
+        message: 'Choose your text color (ex: white, black, red)',
+        name: 'textColor'
+    },
+    {
+        type: 'list',
+        message: 'Choose your shape',
+        name: 'shape',
+        choices: ["Square", "Triangle", "Circle"]
+    },
+    {
+        type: 'input',
+        message: 'Choose your shape color (ex: blue, yellow, red)',
+        name: 'shapeColor'
+    }
+];
+
+inquirer
+    .prompt(questions)
     
-}
+    .then((response) => {
+
+    })
